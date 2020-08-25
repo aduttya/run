49d35519ea0e797a101ddc2660e5e6a49c2c9425
aduttya <anjayyadaav379@gmail.com>
2020-08-25 10:20:08 +0530

    Submission #3 Problem: Wn [4A] Problem Statement: https://codeforces.com/problemset/problem/4/A Comment: modi ji

--- Submissions/Wn [4A].cpp
+++ Submissions/Wn [4A].cpp
@@ -1,8 +1,9 @@
 /**
  *	Author: Ajay_yadav <aduttya>
- *	Date: Tuesday, August 25, 2020   10:19:46 AM
+ *	Date: Tuesday, August 25, 2020   10:20:08 AM
  *	Problem: Wn [4A]
  *	Problem Statement: https://codeforces.com/problemset/problem/4/A
+ *	Comment: modi ji
 **/
 #include<iostream>
 using namespace std;
