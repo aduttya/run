49b6b8a7b780603aa6cc8ded7af615a183e3a8a8
aduttya <anjayyadaav379@gmail.com>
2020-08-25 10:19:46 +0530

    Submission #2 Problem: Wn [4A] Problem Statement: https://codeforces.com/problemset/problem/4/A

---
+++ Submissions/Wn [4A].cpp
@@ -0,0 +1,14 @@
+/**
+ *	Author: Ajay_yadav <aduttya>
+ *	Date: Tuesday, August 25, 2020   10:19:46 AM
+ *	Problem: Wn [4A]
+ *	Problem Statement: https://codeforces.com/problemset/problem/4/A
+**/
+#include<iostream>
+using namespace std;
+
+int main(){
+int a;
+cin>>a;
+cout<<a;
+cout<<"hello";}
