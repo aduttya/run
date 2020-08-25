6f54f1f378704ad64b0ca61ad04fe5ae11243bcc
aduttya <anjayyadaav379@gmail.com>
2020-08-25 10:12:18 +0530

    Submission #1 Problem: Watermelon [4A] Problem Statement: https://codeforces.com/problemset/problem/4/A

---
+++ Submissions/Watermelon [4A].cpp
@@ -0,0 +1,11 @@
+/**
+ *	Author: Ajay_yadav <aduttya>
+ *	Date: Tuesday, August 25, 2020   10:12:18 AM
+ *	Problem: Watermelon [4A]
+ *	Problem Statement: https://codeforces.com/problemset/problem/4/A
+**/
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"hello";}
